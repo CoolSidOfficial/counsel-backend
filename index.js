40fb36a50a74e409305a7241862c59bac84fb950
Coolsidofficial <hackersiddhant1@gmail.com>
2024-09-27 21:45:57 +0530

    basic working of backend

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose=require("mongoose")
 const bodyParser=require("body-parser")
 const app=express()
 const port=3000
-mongoose.connect("mongodb://localhost:27017/mp-dte")
+mongoose.connect("mongodb://localhost:27017/mp-dte",)
 
 const db=mongoose.connection
 db.on("error",()=>{console.log("Error in connecting to database")})
@@ -20,22 +20,36 @@ const formSchema=new mongoose.Schema({
     "TOTAL ALLOTTED": Number,
   }
 )
-const formModel=new mongoose.model("2022_firsts",formSchema)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-async function fucker(r){
-
-    const find_branch=  await formModel.find({"JEE CLOSING RANK":{$lt:r+1}})
-    console.log(find_branch)
-    return   find_branch
+async function Compute(rank,round,category,branch ){
+  let data_round_name="2022_"+"first"+"s"
+  if (round=="1"){
+     data_round_name="2022_"+"first"+"s";
+  }
+  if (round=="2"){
+     data_round_name="2022_"+"second"+"s";
+  }
+  if  (round=="3"){
+    data_round_name="2022_"+"third"+"s";
+  }
+  // console.log(data_round_name) 
+  const formModel=new mongoose.model(data_round_name,formSchema)
+  const find_branchs= await formModel.find({"JEE CLOSING RANK":{$lt:rank+1},branch:branch})
+  console.log(find_branchs)
 }
 app.post("/submit-form", (request,response)=>{  
-  response.send("Form Submitted")
-  console.log(request.body.rank)
-   fucker(request.body.rank)
-  
+  response.status(200)
+  response.send("ok")
+  recv=request.body
+  const {rank,category,round,branch}=recv
+  //  console.log(rank,category)  
+   Compute(rank,round,category,branch)
 
-})
+
+   
+
+}) 
     
 app.listen(port,()=>{
     console.log("Server is running on port 3000")
